75e3129ca4502481a21fd74d8bbc00935f7cabfb
Thiago de Melo Lima <remediosdemaispodcast@gmail.com>
2022-12-29 12:58:44 -0300

    arrumando contador de dias

--- js/site.js
+++ js/site.js
@@ -10,19 +10,19 @@ $(window).on("load", function(){
         }
         });
 
-
-        var dataMenosUmDia = new Date().getFullYear();
+        var dataMenosUmDia = new Date().getFullYear()+1;
 
           $("#MenosUmDia")
-          .countdown(dataMenosUmDia+"/12/31", function (event) {
+          .countdown(dataMenosUmDia+"/01/01", function (event) {
             $(this).html(
               event.strftime('Menos um dia: <del><span>%-D</span></del>')
             );
           });
         
+          var dataTotalDias = new Date().getFullYear();
 
-         var date1 = new Date(dataMenosUmDia+"/01/01");
-        var date2 = new Date(dataMenosUmDia+"/12/31");
+         var date1 = new Date(dataMaisUmDia+"/01/01");
+        var date2 = new Date(dataTotalDias+"/01/01");
         var timeDiff = Math.abs(date2.getTime() - date1.getTime());
         var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
         $('#totalDias').text(diffDays);
