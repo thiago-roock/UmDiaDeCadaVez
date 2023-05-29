a4e0ffca4d6fcc7c354959d198b56d6a4058c843
Thiago de Melo Lima <remediosdemaispodcast@gmail.com>
2023-05-29 15:30:06 -0300

    Reformulação modo escuro e persistencia dos modos

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,24 +1,40 @@
-jQuery(".modoWhite").hide();
-var xx = "corBranca";
-jQuery("#modoWhite").click(function () {
-    mudandoCor();
-})
-jQuery("#modoDark").click(function () {
-    mudandoCor();
-})
-function mudandoCor() {
-    if (xx == "corBranca") {
-        jQuery("#corpo").addClass("bg-dark text-light");
-        jQuery("#rodapeT").removeClass("text-dark");
-        xx = "corPreta";
-        jQuery(".modoDark").hide();
-        jQuery(".modoWhite").show();
+// pegamos o valor no localStorage
+const nightModeStorage = localStorage.getItem('modoDarkStorage')
+const nightMode = document.querySelector('#switch-flat')
 
-    } else {
+// pega o valor do meta tag
+const metaThemeColor = document.querySelector('meta[name=theme-color]')
+
+// caso tenha o valor no localStorage
+if (nightModeStorage) {
+  // ativa o night mode
+  document.documentElement.classList.add('night-mode')
+  jQuery("#corpo").addClass("bg-dark text-light");
+  jQuery("#rodapeT").removeClass("text-dark");
+
+  // já deixa o input marcado como ativo
+  nightMode.checked = true
+}
+
+// ao clicar mudaremos as cores
+nightMode.addEventListener('click', () => {
+  // adiciona a classe `night-mode` ao html
+  document.documentElement.classList.toggle('night-mode')
+
+  // se tiver a classe night-mode
+  if (document.documentElement.classList.contains('night-mode')) {
+    // salva o tema no localStorage
+    jQuery("#corpo").addClass("bg-dark text-light");
+    jQuery("#rodapeT").removeClass("text-dark");
+
+    localStorage.setItem('modoDarkStorage', true)
+
+    return
+  }
+  // senão remove
         jQuery("#corpo").removeClass("bg-dark text-light");
         jQuery("#rodapeT").addClass("text-dark");
-        xx = "corBranca";
         jQuery(".modoDark").show();
         jQuery(".modoWhite").hide();
-    }
-}
+        localStorage.removeItem('modoDarkStorage')
+})
